92f0b2051f4ce0097bd45f7c4c9b69278f9f25d4
fiche-paie <xxxwitter@gmail.com>
2025-11-15 15:32:22 +0100

    Delete debug.php

--- debug.php
+++
@@ -1,7 +0,0 @@
-<?php 
-echo "TEST PHP ACTIF - " . date("H:i:s") . "\n";
-echo "IP: " . $_SERVER["REMOTE_ADDR"] . "\n";
-echo "UA: " . ($_SERVER["HTTP_USER_AGENT"] ?? "none") . "\n";
-require_once "antispam.php";
-echo "Protection passée\n";
-?>
